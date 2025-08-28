a364d06f5a3de2d11f8039f47c8cb890da5d9d54
Imran Khan <imran@isupercoder.com>
2025-08-28 13:10:14 +0500

    feat(todo): add functionality to save todos to a file
    - implement `saveTodo` function to write todos to 'todos.txt'
    - append todos to the file to prevent overwriting existing data on each save

--- todo.py
+++ todo.py
@@ -4,6 +4,7 @@ print("Welcome to our Todo APP :)")
 
 todos = []
 
+
 def addTodo():
     todo = input("Enter todo title: ").strip()
     if todo:
@@ -11,6 +12,7 @@ def addTodo():
         print("Your todo is added to the list.")
     else:
         print("Write a proper todo title.")
+
 
 def showTodos():
     if not todos:
@@ -21,22 +23,32 @@ def showTodos():
             print(f"{i}. {todo}")
     input("\nPress Enter to return to menu...")
 
+
 def showMenu():
-    print("""
+    print(
+        """
     1. Add todo
     2. View all todos
     3. Exit program
-    """)
+    """
+    )
+
+
+def saveTodo():
+    with open("todos.txt", "a") as file:
+        for todo in todos:
+            file.write(todo + "\n")
+
 
 while True:
     showMenu()
     choice = input("Enter choice (1/2/3): ").strip()
 
-    if choice == '1':
+    if choice == "1":
         addTodo()
-    elif choice == '2':
+    elif choice == "2":
         showTodos()
-    elif choice == '3':
+    elif choice == "3":
         print("Goodbye! :)")
         break
     else:
