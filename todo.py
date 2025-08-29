3a38f2f0dab5eca74b378b39c3b1a8efeccc5f89
Imran Khan <imran@isupercoder.com>
2025-08-29 15:17:24 +0500

    feat(todo): add edit and status update functionality   - implement function to mark todos as complete or incomplete   - introduce functionality to edit existing todo titles   - update the todo list display to show completion status
    refactor(persistence): migrate data storage to JSON format
      - change todo data structure from a string to a dictionary
      - replace .txt file storage with a more robust .json format
      - implement load/save functions to persist todos in JSON

--- todo.py
+++ todo.py
@@ -1,17 +1,61 @@
 # A simple Todo list
+import json
 
 print("Welcome to our Todo APP :)")
 
-todos = []
+
+def loadTodos():
+    global todos
+    try:
+        with open("todos.json", "r") as file:
+            todos = json.load(file)
+    except FileNotFoundError:
+        todos = []
+
+
+loadTodos()
 
 
 def addTodo():
-    todo = input("Enter todo title: ").strip()
-    if todo:
-        todos.append(todo)
+    title = input("Enter todo title: ").strip()
+    if title:
+        todos.append({"title": title, "done": False})
         print("Your todo is added to the list.")
+        saveTodo()
     else:
         print("Write a proper todo title.")
+
+
+def markTodo():
+    showTodos()
+    try:
+        num = int(input("Enter the number of todo you want to update status for: "))
+        if 1 <= num <= len(todos):
+            todos[num - 1]["done"] = not todos[num - 1]["done"]
+            saveTodo()
+        else:
+            print("invalid Number")
+    except ValueError:
+        print("Please enter a valid number")
+
+
+def editTodo():
+    showTodos()
+    try:
+        num = int(input("Enter the number of todo you want to edit: "))
+        if 1 <= num <= len(todos):
+            new_title = input("Enter new title for selected Todo: ").strip()
+            if new_title:
+                todos[num - 1]["title"] = new_title
+                saveTodo()
+                print("Todo edited successfully!")
+            else:
+                print("Title can't be empty...")
+        else:
+            print("invalid number")
+
+    except ValueError:
+        print("Enter proper number to select todo")
 
 
 def showTodos():
@@ -20,7 +64,8 @@ def showTodos():
     else:
         print("\nYour Todos:")
         for i, todo in enumerate(todos, 1):
-            print(f"{i}. {todo}")
+            status = "[x]" if todo["done"] else "[ ]"
+            print(f"{i}. {status} {todo['title']}")
     input("\nPress Enter to return to menu...")
 
 
@@ -35,9 +80,8 @@ def showMenu():
 
 
 def saveTodo():
-    with open("todos.txt", "a") as file:
-        for todo in todos:
-            file.write(todo + "\n")
+    with open("todos.json", "w") as file:
+        json.dump(todos, file)
 
 
 while True:
